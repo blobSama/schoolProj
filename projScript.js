7735ed882dba4de4c7f9036942edcc8e2cefba41
blobSama <66427163+blobSama@users.noreply.github.com>
2020-11-02 22:18:38 +0200

    added script file, stated imput check

---
+++ projScript.js
@@ -0,0 +1,16 @@
+// JavaScript source file
+function checkForm() {
+    if (!checkPic) {
+        document.write(whoops, sorry);
+    }
+}
+
+function checkPic(getElementById(myfile)) {
+    if (typeof.getElementByType(myfile)==".jpg" {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
